87ffd64f2af25dffece2de52a52a01a2012a0ba7
Erbol <erbolclassic1297@gmail.com>
2023-02-09 12:26:38 +0500

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,26 @@ submit.addEventListener('click', () => {
       return data.json();
     })
     .then((data) => renderAnswer(data))
-    .catch((err) => alert(err));
+    .catch((err) => {
+      messageContainer.innerHTML += `<div class="chat-message-left pb-4">
+                                      <div>
+                                        <img
+                                          src="https://bootdey.com/img/Content/avatar/avatar3.png"
+                                          class="rounded-circle mr-1"
+                                          alt="AI"
+                                          width="40"
+                                          height="40" />
+                                        <div class="text-muted small text-nowrap mt-2">${new Date().toLocaleTimeString()}</div>
+                                      </div>
+                                      <div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
+                                        <div class="font-weight-bold mb-1">AI</div>
+                                        <pre class="answerBody">The request was sent with an error, please try again
+                                          </br>
+                                          Error Message: ${err}
+                                        </pre>
+                                      </div>
+                                    </div>`;
+    });
   renderQuestion();
   lastQuestionTemplate();
   question.value = '';
@@ -37,7 +56,7 @@ function renderAnswer(data) {
                             <img
                               src="https://bootdey.com/img/Content/avatar/avatar3.png"
                               class="rounded-circle mr-1"
-                              alt="Sharon Lessman"
+                              alt="AI"
                               width="40"
                               height="40" />
                             <div class="text-muted small text-nowrap mt-2">${new Date().toLocaleTimeString()}</div>
@@ -55,7 +74,7 @@ function renderQuestion() {
                 <img
                   src="https://bootdey.com/img/Content/avatar/avatar1.png"
                   class="rounded-circle mr-1"
-                  alt="Chris Wood"
+                  alt="User"
                   width="40"
                   height="40" />
                 <div class="text-muted small text-nowrap mt-2">${new Date().toLocaleTimeString()}</div>
